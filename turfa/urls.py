76a266b35eb83cbd52ef9f462adda3507c5982d7
Ubuntu <ubuntu@ip-172-31-0-8.ec2.internal>
2023-02-28 07:39:33 +0000

    last version 28.02

--- turfa/urls.py
+++ turfa/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,9 +24,12 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-)
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
 else:
     urlpatterns += staticfiles_urlpatterns()
